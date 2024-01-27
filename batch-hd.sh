93970a087ef5d76351ed3c169350abf583ae08be
Elise Haldane <107867060+elisehaldane@users.noreply.github.com>
2024-01-27 18:01:48 +0000

    Add HD scales

--- batch-hd.sh
+++ batch-hd.sh
@@ -4,6 +4,7 @@ set -e
 # https://mango.blender.org/download/
 curl -C - -LO https://ftp.nluug.nl/pub/graphics/blender/demo/movies/ToS/tearsofsteel_4k.mov
 
+input=tearsofsteel_4k.mov
 probe_input() {
     ffprobe -v error \
         -select_streams v:0 \
@@ -11,7 +12,7 @@ probe_input() {
         -of default=nokey=1:noprint_wrappers=1 $1
 }
 
-err=$(probe_input tearsofsteel_4k.mov)
+err=$(probe_input $input)
 if [[ $(echo $err) != 'h264' ]]; then
     echo "[err] download appears corrupt"
     exit 1  # Bail
@@ -19,18 +20,19 @@ else
     echo "[info] download appears valid"
 fi
 
-benchmark_1080p() {
-    echo "[info] benchmarking $1 for $input"
+benchmark_scales() {
+    echo "[info] benchmarking $1 for $input at "$scale"p"
     ffmpeg -threads $(nproc) -benchmark -i $input -c:v $1 \
-        -start_number 1 -vframes 1000 -vf scale="-2:1080" \
+        -start_number 1 -vframes 1000 -vf scale="$scale" \
         -crf $2 -preset slow -c:a copy -f null out.null 2> \
-        $(date +%s)-tos-ffmpeg-$(nproc)-$1-slow.log
+        $(date +%s)-tos-"$scale"p-ffmpeg-$(nproc)-$1-slow.log
 }
 
-for input in *.mov; do
-    benchmark_1080p libx264 23
-    benchmark_1080p libx265 28
-    echo "[info] benchmarking completed for $input"
+scales=(1920:1080 1280:720)
+for scale in ${scales[@]}; do
+    benchmark_scales libx264 23
+    benchmark_scales libx265 28
+    echo "[info] benchmarking completed for $input at "$scale"p"
 done
 
 echo "[info] all benchmarking completed"
